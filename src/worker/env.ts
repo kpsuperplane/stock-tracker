8a80ed2d2e1cf5fd9af46fffa9ac646ce789d4bd
Kevin Pei <hello@kevinpei.com>
2026-07-09 23:23:48 -0400

    chore: declare required worker secrets

--- src/worker/env.ts
+++ src/worker/env.ts
@@ -2,6 +2,4 @@ import type { ScreeningJobMessage } from "../shared/contracts";
 
 export interface Env extends Cloudflare.Env {
   SCREENING_QUEUE: Queue<ScreeningJobMessage>;
-  BASIC_AUTH_USERNAME: string;
-  BASIC_AUTH_PASSWORD: string;
 }
